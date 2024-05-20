b986bf9bd20c27913bc3910cba09b9f8b2e3d4b9
ldgeyexea <piotrek269962@gmail.com>
2024-05-20 17:08:30 +0200

    moved branches

--- cllvm/src/main/java/com/at24/translationTools/TypeTranslator.java
+++ cllvm/src/main/java/com/at24/translationTools/TypeTranslator.java
@@ -6,17 +6,20 @@ public class TypeTranslator {
         switch (typeName) {
             case "int":
                 return "i32";
+            case "long":
+                return "i64";
+            case "short":
+                return "i16";
             case "float":
                 return "f32";
             case "double":
                 return "f64";
-            case "short":
-                return "i16";
             case "void":
                 return "void";
+            case "char":
+                return "i8";
         }
-      return "unspecified";
+      return null;
     };
-    //type
 
 }
