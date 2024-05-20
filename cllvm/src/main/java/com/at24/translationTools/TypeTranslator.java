677a9064409dad0f531a7e4ddd1e39f88eedeb62
ldgeyexea <piotrek269962@gmail.com>
2024-05-20 16:50:01 +0200

    type converter created

--- cllvm/src/main/java/com/at24/translationTools/TypeTranslator.java
+++ cllvm/src/main/java/com/at24/translationTools/TypeTranslator.java
@@ -1,5 +1,20 @@
 package com.at24.translationTools;
 
 public class TypeTranslator {
+    public static String typeConverter(String typeName){
+        switch (typeName) {
+            case "int":
+                return "i32";
+            case "float":
+                return "f32";
+            case "double":
+                return "f64";
+            case "short":
+                return "i16";
+            case "void":
+                return "void";
+        }
+      return "unspecified";
+    };
     
 }
