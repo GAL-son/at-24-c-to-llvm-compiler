5ec73cf4f00ecbfa2de6ff2f78260d42232b1d2f
ldgeyexea <piotrek269962@gmail.com>
2024-05-28 23:00:48 +0200

    jsonvisitor for additions and multiplications

--- cllvm/src/main/java/com/at24/Main.java
+++ cllvm/src/main/java/com/at24/Main.java
@@ -9,7 +9,7 @@ import com.at24.visitors.JSONVisitor;
 
 public class Main {
     public static void main(String[] args) {
-        String code = "const int test = 10;";
+        String code = "const int test = 10;int a = 5; int c=1*5+8;";
 
         CharStream stream = CharStreams.fromString(code);
 
